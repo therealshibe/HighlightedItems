616b711e51146c257fbab41cd2e6519a364ae7df
TehCheat <thecheatuc@gmail.com>
2021-07-30 15:32:28 -0400

    added a check for full inventory and a few helpful messages.

--- HighlightedItems.cs
+++ HighlightedItems.cs
@@ -74,7 +74,7 @@ namespace HighlightedItems
                 }
 
                 string countText;
-                if (Settings.ShowStackSizes && highlightedItems.Count != stackSizes && stackSizes !=null)
+                if (Settings.ShowStackSizes && highlightedItems.Count != stackSizes && stackSizes != null)
                     if (Settings.ShowStackCountWithSize) countText = $"{stackSizes} / {highlightedItems.Count}";
                     else countText = $"{stackSizes}";
                 else
@@ -90,10 +90,22 @@ namespace HighlightedItems
                     var prevMousePos = Mouse.GetCursorPosition();
                     foreach (var item in highlightedItems)
                     {
-                        if (stashPanel.IsVisible)
+                        if (!ingameState.IngameUi.StashElement.IsVisible)
                         {
-                            moveItem(item.GetClientRect().Center);
+                            DebugWindow.LogMsg("HighlightedItems: Stash Panel closed, aborting loop");
+                            break;
                         }
+                        if (!ingameState.IngameUi.InventoryPanel.IsVisible)
+                        {
+                            DebugWindow.LogMsg("HighlightedItems: Inventory Panel closed, aborting loop");
+                            break;
+                        }
+                        if (IsInventoryFull())
+                        {
+                            DebugWindow.LogMsg("HighlightedItems: Inventory full, aborting loop");
+                            break;
+                        }
+                        moveItem(item.GetClientRect().Center);
                     }
                     Mouse.moveMouse(prevMousePos);
                 }
@@ -113,8 +125,19 @@ namespace HighlightedItems
                 {
                     foreach (var item in inventoryItems)
                     {
-                        if (!CheckIgnoreCells(item) && inventoryPanel.IsVisible && stashPanel.IsVisible)
+                        if (!CheckIgnoreCells(item))
                         {
+                            if (!ingameState.IngameUi.InventoryPanel.IsVisible)
+                            {
+                                DebugWindow.LogMsg("HighlightedItems: Inventory Panel closed, aborting loop");
+                                break;
+                            }
+                            
+                            if (!ingameState.IngameUi.StashElement.IsVisible)
+                            {
+                                DebugWindow.LogMsg("HighlightedItems: Stash Panel closed, aborting loop");
+                                break;
+                            }
                             moveItem(item.GetClientRect().Center);
                         }
                     }
@@ -149,6 +172,51 @@ namespace HighlightedItems
             }
 
             return highlightedItems;
+        }
+
+        private bool IsInventoryFull()
+        {
+            var inventoryPanel = ingameState.IngameUi.InventoryPanel;
+            var inventoryItems = inventoryPanel[InventoryIndex.PlayerInventory].VisibleInventoryItems;
+
+            // quick sanity check
+            if (inventoryItems.Count < 12)
+            {
+                return false;
+            }
+
+            // track each inventory slot
+            bool[,] inventorySlot = new bool[12, 5];
+
+            // iterate through each item in the inventory and mark used slots
+            foreach (var inventoryItem in inventoryItems)
+            {
+                int x = inventoryItem.InventPosX;
+                int y = inventoryItem.InventPosY;
+                int height = inventoryItem.ItemHeight;
+                int width = inventoryItem.ItemWidth;
+                for (int row = x; row < x + width; row++)
+                {
+                    for (int col = y; col < y + height; col++)
+                    {
+                        inventorySlot[row, col] = true;
+                    }
+                }
+            }
+
+            // check for any empty slots
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if (inventorySlot[x, y] == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+            // no empty slots, so inventory is full
+            return true;
         }
 
         public void moveItem(SharpDX.Vector2 itemPosition)
